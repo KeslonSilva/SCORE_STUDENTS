459236b04a3ad5fddf710019f6a7b6aae42a0a58
Keslon Silva <kesloni40@gmail.com>
2023-06-09 23:50:24 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+student_scores = [25, 87, 19, 54, 47, 34, 56, 89, 74]
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+print(f"The highest score in the class is: {highest_score}")
+    
+    
+
+
+
